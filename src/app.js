44c581943a7721e688b89feaea53cfe1a8a15e3c
sswq2020 <a309108243@qq.com>
2020-05-20 16:16:37 +0800

    feat: 🎸 登陆与注册的模板渲染由routes下的view负责渲染

--- src/app.js
+++ src/app.js
@@ -13,8 +13,7 @@ const { REDIS_CONF } = require('./conf/db')
 const { isProd } = require('./utils/env')
 
 const index = require('./routes/index')
-const users = require('./routes/users')
-
+const userViewRouter = require('./routes/view/user')
 const errorViewRouter = require('./routes/view/error')
 
 // error handler 
@@ -63,8 +62,7 @@ app.use(session({
 // routes
 
 app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-
+app.use(userViewRouter.routes(), index.allowedMethods())
 app.use(errorViewRouter.routes(), index.allowedMethods()) // 404路由一定放到最后 
 
 // error-handling
