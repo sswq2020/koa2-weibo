ecef837c0fe645a8b4a5e61bf5ec9065477569f6
sswq2020 <a309108243@qq.com>
2020-05-23 09:04:00 +0800

    feat: 🎸 判断已登录情况下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,12 +5,27 @@
 
 const router = require('koa-router')()
 
+const getSessionUserInfo = (ctx)=>{
+    const userInfo = ctx.session.userInfo
+    if(!userInfo){
+        return {
+            isLogin:false
+        }
+    }else{
+        return {
+            isLogin:true,
+            userName:userInfo.userName
+        }
+    }
+}
+
+
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    await ctx.render('login',getSessionUserInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', getSessionUserInfo(ctx))
 })
 
 module.exports = router
