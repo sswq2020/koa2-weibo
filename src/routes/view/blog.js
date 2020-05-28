a678259edec1227053ead9a00c67c5fd09bdf482
sswq2020 <a309108243@qq.com>
2020-05-28 14:49:59 +0800

    fix: 🐛 判断个人主页是否存在的业务逻辑

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -7,6 +7,8 @@ const router = require('koa-router')()
 
 const { genLoginRedirect } = require('../../middlewares/loginChecks')
 const { getProfileBlogList } = require('../../controller/blog-profile')
+const { isExist } = require('../../controller/user')
+const {ERR_OK} = require('../../conf/constant')
 
 // 首页
 router.get('/', genLoginRedirect(), async (ctx, next) => {
@@ -21,9 +23,23 @@ router.get('/profile', genLoginRedirect(), async (ctx, next) => {
 
 // 个人主页
 router.get('/profile/:userName', genLoginRedirect(), async (ctx, next) => {
-    debugger
     const profileUserName = ctx.params.userName
-    const userInfo = ctx.session.userInfo
+    const myUserInfo = ctx.session.userInfo
+    const isMe = profileUserName === myUserInfo.userName
+    let curUserInfo
+    if(isMe){
+        curUserInfo = myUserInfo
+    } else {
+        // 不是当前登录用户
+        const existResult = await isExist
+        if(existResult === ERR_OK){
+            curUserInfo = existResult.data
+        }else {
+            // 用户名不存在
+            return
+        }
+    }
+
     const res = await getProfileBlogList(profileUserName)
     const {
         blogList,
@@ -42,8 +58,8 @@ router.get('/profile/:userName', genLoginRedirect(), async (ctx, next) => {
             isEmpty
         },
         userData: {
-            userInfo,
-            isMe: profileUserName === userInfo.userName
+            userInfo: curUserInfo,
+            isMe
         }
     })
 })
