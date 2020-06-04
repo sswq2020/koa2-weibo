76ca1dcf8cfbfe9efd802cf32952b4be40e0315c
sswq2020 <a309108243@qq.com>
2020-06-04 19:09:42 +0800

    test: 💍 微博主页微博列表测试成功

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -7,12 +7,12 @@
  * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
  * 每次测试用户重新登录，都需要更新这里的 cookie
  */
-const S_COOKIE = 'weibo.sid=RjUybioYBm6bUHs8eI4MBmw9w8n2jRYc; weibo.sid.sig=VQowBXj4KZBU_fWE32jgnhVhuu8'
+const S_COOKIE = 'weibo.sid=NkmdpD2PsEF-sIHL4DgUeJUJVWJGi4WN; weibo.sid.sig=dh8bNVky3kYLF7upw2-19u6Mthk'
 
-const V_COOKIE = 'weibo.sid=jcSEvv5hGWv5N5Qbf6AXKRkRQqjGX-K1; weibo.sid.sig=cOxDg_gggGecZEoCpvzDTBebtNw'
+const V_COOKIE = 'weibo.sid=3SMJT_n2P5tmXw7PBX1YjSS1py_wS9b8; weibo.sid.sig=RVJmtjyJUDjoRGXFs9CvabVspso'
 
 module.exports = {
-    S_ID:3,
+    S_ID:1,
     S_COOKIE,
     S_USER_NAME:'sswq',
 
